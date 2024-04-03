53221e167a5e54683ecb8d20067790ea3431a752
Myungbin <mbgwak1103@gmail.com>
2024-04-03 17:04:20 +0900

    T

--- ImageNet/main.py
+++ ImageNet/main.py
@@ -13,14 +13,15 @@ from ImageNet.utils.logging import set_logging
 
 warnings.filterwarnings(action="ignore")
 
-set_logging()
 
-dataloader = DatasetLoader(num_workers=CFG.NUM_WORKERS)
-train_loader, val_loader = dataloader.load
-model = VisionTransformer(num_classes=CFG.NUM_CLASS)
-scaler = torch.cuda.amp.GradScaler()
-criterion = nn.CrossEntropyLoss()
-optimizer = AdamW(model.parameters(), lr=CFG.LEARNING_RATE)
-scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2, eta_min=0, verbose=True)
-trainer = Trainer(model, criterion, optimizer, scheduler, scaler, True)
-trainer.fit(train_loader, val_loader)
+if __name__ == "__main__":
+    set_logging()
+    dataloader = DatasetLoader(num_workers=CFG.NUM_WORKERS)
+    train_loader, val_loader = dataloader.load
+    model = VisionTransformer(num_classes=CFG.NUM_CLASS)
+    scaler = torch.cuda.amp.GradScaler()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = AdamW(model.parameters(), lr=CFG.LEARNING_RATE)
+    scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2, eta_min=0, verbose=True)
+    trainer = Trainer(model, criterion, optimizer, scheduler, scaler, True)
+    trainer.fit(train_loader, val_loader)
